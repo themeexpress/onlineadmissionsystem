b641486d3c03f2edcc030136443fe7f08834919c
kamrul <kamrul_bd_islam@yahoo.com>
2018-01-11 16:09:31 +0600

    User Profile design Done
    Iconic Design in User profile Done!!

--- dashboard/userprofile.php
+++ dashboard/userprofile.php
@@ -7,8 +7,23 @@
                 <div class="block"> 
                     <div class="container">
                      <div class="row">
-                         <div class="col-md-10">
-                             <i class="fa fa-trash" aria-hidden="true"></i>
+                         <div class="col-md-3">
+                            <img class="img-responsive" style="max-width: 150px;  max-height: 101px; margin: auto;" src="img/application.png">
+                            <h3 class="text-center">Application Status</h3>
+                            <p class="text-center">your application status is: </p>
+                            <a class="btn btn-primary text-center" style="float:right;padding: 0 25px;" href="status.php">STATUS</a>
+                            
+
+                         </div>
+                         <div class="col-md-3">
+                            <img class="img-responsive uicon" style="max-width: 150px;  max-height: 101px; margin: auto;" src="img/admit.png">
+                            <h3 class="text-center">Collect Admit Card</h3>
+                            <p class="text-center">Collect Admit Card to seat in Admission Test </p>
+                         </div>
+                         <div class="col-md-3">
+                            <img class="img-responsive uicon" style="max-width: 150px;  max-height: 101px; margin: auto;" src="img/result.png">
+                            <h3 class="text-center">Admission Result</h3>
+                            <p class="text-center">Admission Merit List and Wating Lists </p>
                          </div>
                      </div>   
                     </div>
